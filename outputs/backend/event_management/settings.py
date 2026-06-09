394e50a4526179ffbadf682b8a92bab51a9e5c81
Abinaya R <your-github-email@example.com>
2026-06-09 09:41:29 +0530

    database

--- outputs/backend/event_management/settings.py
+++ outputs/backend/event_management/settings.py
@@ -64,13 +64,14 @@ WSGI_APPLICATION = "event_management.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "event_management"),
+        "NAME": os.getenv("DB_NAME", "EventmanagementCIT"),
         "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "Abinaya18@"),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
+
 
 AUTH_USER_MODEL = "users.User"
 
